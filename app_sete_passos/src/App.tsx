84a2c7911b306668e3aaba9a35d238c462392e31
Gabriel Almeida <gabrielalmeida2612@outlook.com>
2022-07-06 16:41:09 -0300

    Consertando um erro

--- app_sete_passos/src/App.tsx
+++ app_sete_passos/src/App.tsx
@@ -4,16 +4,16 @@ import Routes from './routes';
 import {AutenticacaoProvider} from './contexts/AutenticacaoContext';
 import {PesquisaProvider} from './contexts/PesquisaContext';
 import {CarrinhoProvider} from './contexts/CarrinhoContext';
-//import { FavoritesProvider } from './contexts/FavoritesContext';
+import { FavoritesProvider } from './contexts/FavoritesContext';
 
 export default () => {
   return (
     <AutenticacaoProvider>
       <PesquisaProvider>
         <CarrinhoProvider>
-          {/* <FavoritesProvider> */}
+          <FavoritesProvider>
           <Routes />
-          {/* </FavoritesProvider> */}
+          </FavoritesProvider>
        </CarrinhoProvider>
       </PesquisaProvider>
     </AutenticacaoProvider>
